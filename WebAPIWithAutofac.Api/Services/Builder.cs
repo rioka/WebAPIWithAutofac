8de1fc9b4456a0b3647d6ee6d6262827c7a77086
rioka <rioka@libero.it>
2019-08-17 16:42:11 +0100

    Add IBuilder and Builder

---
+++ WebAPIWithAutofac.Api/Services/Builder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace WebAPIWithAutofac.Api.Services
+{
+  public class Builder : IBuilder, IDisposable
+  {
+    private readonly int _value;
+    public Builder(int value)
+    {
+      _value = value;
+    }
+
+    public string Build()
+    {
+      return $"Built {_value}";
+    }
+
+    public void Dispose()
+    {
+      Debug.WriteLine($"Disposing a {nameof(Builder)} instance, created with {_value}");
+    }
+  }
+}
